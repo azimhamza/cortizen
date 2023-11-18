00f6718e3409b9a6d0d6ede7e45e756b17aa0790
Soham Basu <sohambasu963@gmail.com>
2023-11-18 16:35:19 -0500

    Fix formatting issues and add Twilio integration

---
+++ lib/features/twilio.dart
@@ -0,0 +1,15 @@
+// lib/features/twilio.dart
+import 'package:twilio_flutter/twilio_flutter.dart';
+
+void sendTextMessage(String toNumber, String messageBody) async {
+  TwilioFlutter twilioFlutter = TwilioFlutter(
+    accountSid: 'AC157612dd88c9b68918559b02556cf275',
+    authToken: '22dd8a35a84209968156e8e34ff7128f',
+    twilioNumber: '+13239634972'
+  );
+
+  await twilioFlutter.sendSMS(
+    toNumber: toNumber,
+    messageBody: messageBody,
+  );
+}
